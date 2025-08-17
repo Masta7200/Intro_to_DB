545b56d364ce984059ac36db49bd75537b7b85f5
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-08-17 17:22:15 +0200

    select queries added

---
+++ task_4.sql
@@ -0,0 +1,11 @@
+SELECT 
+    COLUMN_NAME AS 'Column Name', 
+    COLUMN_TYPE AS 'Column Type', 
+    IS_NULLABLE AS 'Is Nullable', 
+    COLUMN_DEFAULT AS 'Default Value', 
+    EXTRA AS 'Extra Info'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_NAME = 'Books' 
+    AND TABLE_SCHEMA = 'alx_book_store';
